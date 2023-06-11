035692f31cd8c79c051fa63a5786589b74d1c05f
inclinedadarsh <dubeyadarshmain@gmail.com>
2023-06-11 21:55:03 +0530

    feat: setup app router using react-router-dom
    setup routes for different path in react router dom. Created component files for each route. Created index.js in components to serve those files. Used Routes to use those files. Also used <Box> from @mui/material.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import {
+	Navbar,
+	Feed,
+	VideoDetail,
+	ChannelDetail,
+	SearchFeed,
+} from './components';
+
 const App = () => {
-	return <h1>Hello World!</h1>;
+	return (
+		<BrowserRouter>
+			<Box sx={{ background: '#000' }}>
+				<Navbar />
+				<Routes>
+					<Route path="/" exact element={<Feed />} />
+					<Route path="/video/:id" element={<VideoDetail />} />
+					<Route path="/channel/:id" element={<ChannelDetail />} />
+					<Route
+						path="/search/:searchTerm"
+						element={<SearchFeed />}
+					/>
+				</Routes>
+			</Box>
+		</BrowserRouter>
+	);
 };
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import ChannelDetail from './ChannelDetail';
+import Feed from './Feed';
+import Navbar from './Navbar';
+import SearchFeed from './SearchFeed';
+import VideoDetail from './VideoDetail';
+
+export { ChannelDetail, Feed, Navbar, SearchFeed, VideoDetail };
